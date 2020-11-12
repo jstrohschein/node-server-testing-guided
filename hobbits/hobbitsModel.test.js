c1fc413fb9d8e3eb4c03eb57fdb63addb3646c3f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-12 18:57:36 +0100

    wip

--- hobbits/hobbitsModel.test.js
+++ hobbits/hobbitsModel.test.js
@@ -3,5 +3,9 @@ const db = require('../data/dbConfig.js')
 
 // each test starts with the SAME database
 beforeEach(async () => {
+  await db('hobbits').truncate()
+})
+
+describe('hobbits model', () => {
   
 })
